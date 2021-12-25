3678ffdcb061d0fbe1f39a02ce7de9a5d2a6c5b2
mukhtar-github <mukhtartanimugarba@gmail.com>
2021-12-25 02:52:57 +0000

    Update changes

--- backend/index.js
+++ backend/index.js
@@ -19,6 +19,7 @@ MongoClient.connect(
         process.exit(1)
 })
 .then(async client => {
+	await VehiclesDAO.injectDB(client)
         app.listen(port, () => {
                 console.log(`listening on port ${port}`)
         })
